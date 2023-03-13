3c43fe83d606a88bc3983805508df790369e5e10
Sharon Nderi <nderisharonw@gmail.com>
2023-03-13 15:26:38 +0300

    User Model

---
+++ models/user.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""Module for a SQLAlchemy model named User for a database table named users.
+"""
+
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
+
+
+Base = declarative_base()
+
+
+class User(Base):
+    """Model for a database table users."""
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True)
+    email = Column(String(250), nullable=False)
+    username = Column(String(250), nullable=True)
+    hashed_password = Column(String(250), nullable=False)
